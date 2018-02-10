48ef7a43b146d6ff3497cfd9598e36ac58105022
Conlon Novak <conlonn@andrew.cmu.edu>
2018-02-10 12:06:09 -0500

    moved shit

---
+++ static/scripts/scripts.js
@@ -0,0 +1,36 @@
+function searchArtist(){
+	var artistName = $("#searchfield").val()
+	var link = "http://127.0.0.1:5000/api/search_by_artist/" + artistName;
+	//var link = "/api/search_by_artist/" + artistName;
+	//var link = "templates/concerts_dummy_pittsburgh.json"
+	var client = new HttpClient();
+	client.get(link, function(response) {
+    	var obj = JSON.parse(response);
+	});	
+
+	console.log(obj.resultsPage);
+    window.location.replace("http://stackoverflow.com");
+}
+
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );            
+        //anHttpRequest.send( null );
+    }
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function crunchNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
